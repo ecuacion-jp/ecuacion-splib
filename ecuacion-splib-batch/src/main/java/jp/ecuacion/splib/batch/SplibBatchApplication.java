70428bc138c9f92bde7e8450f32c798e5772ab49
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-04-19 12:39:59 +0900

    #96-additional: unneeded Exception throw declaration removed.

--- ecuacion-splib-batch/src/main/java/jp/ecuacion/splib/batch/SplibBatchApplication.java
+++ ecuacion-splib-batch/src/main/java/jp/ecuacion/splib/batch/SplibBatchApplication.java
@@ -10,8 +10,13 @@ public abstract class SplibBatchApplication {
 
   /**
    * Is the main method of the spring batch.
+   * 
+   * <p>java command doesn't seem to start 
+   *     by calling the main method in the parent class of the class specified.<br>
+   *     So you need to implement main mathod in the BatchApplication class in your each app
+   *     and call {@code SplibBatchApplication.main(args)} in it.</p>
    */
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     ConfigurableApplicationContext context =
         SpringApplication.run(SplibBatchApplication.class, args);
     System.exit(SpringApplication.exit(context, () -> 0));
