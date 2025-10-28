5a8f8a533de3dfe47204bfae9f615082d3b7f268
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-10-29 08:32:58 +0900

    insertOrUpdate method introduced to SplibEditJpaService

--- ecuacion-splib-web-jpa/src/main/java/jp/ecuacion/splib/web/jpa/service/SplibEditJpaService.java
+++ ecuacion-splib-web-jpa/src/main/java/jp/ecuacion/splib/web/jpa/service/SplibEditJpaService.java
@@ -18,6 +18,7 @@ package jp.ecuacion.splib.web.jpa.service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jp.ecuacion.lib.jpa.entity.EclibEntity;
+import jp.ecuacion.splib.jpa.repository.SplibRepository;
 import jp.ecuacion.splib.web.form.SplibEditForm;
 import jp.ecuacion.splib.web.service.SplibEditService;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,4 +35,32 @@ public abstract class SplibEditJpaService<F extends SplibEditForm, E extends Ecl
 
   @PersistenceContext
   protected EntityManager em;
+
+  /**
+   * Offers a utility function to upsert database safely in the library.
+   * 
+   * <p>In jpa or spring data jpa world, what you have to do to insert is to 
+   *     {@code repository.save(e)}.<br>
+   *     And no need to do anything to update thanks to "dirty checking"
+   *     (jpa automatically detects and updates changes of managed entities on commit).<br><br>
+   *     But the feature of the library conflicts with dirty checking fefature.<br>
+   *     The library supports "soft delete flag" feature, which should delete key-duplicated 
+   *     soft-deleted record on insert or update.<br><br>
+   *     We can hook "insert" and delete duplicated soft-deleted record
+   *     because "save()" method is always called (it's done in {@code SplibSoftDeleteAdvice}), 
+   *     but "update" we cannot because no method is called 
+   *     during the update derived from "dirty checking".<br><br>
+   *     To prevent primary key duplication (which does not happen in principle 
+   *     because the library needs surrogate key strategy) or unique key dupliication on update
+   *     in the library environment, we need to use {@code save()} to realize hook.<br>
+   *     But it's not enough because when you call {@code save()}, 
+   *     updates derived from "dirty checking" processed right before the hook procedure processed.
+   *     That's why in this method "detach()" firstly to disable "dirty checking",
+   *     then save to prevent key duplication on update.
+   * </p>
+   */
+  protected <T> void insertOrUpdate(SplibRepository<T, Long> repo, T e) {
+    em.detach(e);
+    repo.save(e);
+  }
 }
