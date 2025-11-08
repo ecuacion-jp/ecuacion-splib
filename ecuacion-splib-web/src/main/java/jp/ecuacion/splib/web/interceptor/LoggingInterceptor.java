8e7eca3ce8f67d12f3dce9a2fadd11fa776900a6
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-11-08 13:33:40 +0900

    loglevel of LoggingInterceptor changed to debug.

--- ecuacion-splib-web/src/main/java/jp/ecuacion/splib/web/interceptor/LoggingInterceptor.java
+++ ecuacion-splib-web/src/main/java/jp/ecuacion/splib/web/interceptor/LoggingInterceptor.java
@@ -17,7 +17,7 @@ public class LoggingInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
-    detailLog.info("session ID: " + request.getSession().getId() + ", thread ID: "
+    detailLog.debug("session ID: " + request.getSession().getId() + ", thread ID: "
         + Thread.currentThread().threadId() + " : request process started. request: "
         + request.getRequestURI());
 
@@ -27,14 +27,14 @@ public class LoggingInterceptor implements HandlerInterceptor {
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
       @Nullable ModelAndView modelAndView) throws Exception {
-    detailLog.info("session ID: " + request.getSession().getId() + ", thread ID: "
+    detailLog.debug("session ID: " + request.getSession().getId() + ", thread ID: "
         + Thread.currentThread().threadId() + " : request process finished.");
   }
 
   @Override
   public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
       Object handler, Exception ex) throws Exception {
-    detailLog.info("session ID: " + request.getSession().getId() + ", thread ID: "
+    detailLog.debug("session ID: " + request.getSession().getId() + ", thread ID: "
         + Thread.currentThread().threadId() + " : view rendering finished.");
   }
 }
