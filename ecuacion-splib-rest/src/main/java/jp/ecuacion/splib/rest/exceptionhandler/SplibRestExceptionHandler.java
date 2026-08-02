cd8844020ff164f748c8dc6e317e343615e5e4ab
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-08-02 21:38:17 +0900

    splib-rest: exception handler moved to appropriate package

--- ecuacion-splib-rest/src/main/java/jp/ecuacion/splib/rest/exceptionhandler/SplibRestExceptionHandler.java
+++ ecuacion-splib-rest/src/main/java/jp/ecuacion/splib/rest/exceptionhandler/SplibRestExceptionHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jp.ecuacion.splib.rest.advice;
+package jp.ecuacion.splib.rest.exceptionhandler;
 
 import java.util.List;
 import java.util.Objects;
